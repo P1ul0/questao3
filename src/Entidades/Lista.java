1b2d373e0908020ae7a007cf3981458a635ce0c5
eryckson <eryckson.eag@gmail.com>
2022-10-07 20:43:44 -0300

    inicial

---
+++ src/Entidades/Lista.java
@@ -0,0 +1,71 @@
+package Entidades;
+
+public class Lista {
+    private Node head;
+    private Node tail;
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void setTail(Node tail) {
+        this.tail = tail;
+    }
+
+    public Lista() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void inserir(int newValue) {
+        Node node = new Node(newValue);
+        Node p = this.head;
+        if (this.head == null) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            while (p != null && node.getValue() > p.getValue()) {
+                p = p.getNext();
+            }
+            if (p == null) {
+                this.tail.setNext(node);
+                node.setPrevious(this.tail);
+                this.tail = node;
+            } else if (p.equals(this.head)) {
+                this.head.setPrevious(node);
+                node.setNext(this.head);
+                this.head = node;
+            } else {
+                node.setNext(p);
+                node.setPrevious(p.getPrevious());
+                p.getPrevious().setNext(node);
+                p.setPrevious(node);
+            }
+
+        }
+
+    }
+    public void limpar(Lista l){
+        l.setHead(null);
+        l.setTail(null);
+    }
+
+    public void copiar(){
+
+    }
+    public void print(){
+        Node p = this.head;
+        while (p != null){
+            System.out.println(p.getValue());
+            p =p.getNext();
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import Entidades.Lista;
+
+public class Main {
+    public static void main(String[] args) {
+        Lista l = new Lista();
+        l.inserir(1);
+        l.inserir(3);
+        l.inserir(2);
+        l.inserir(4);
+        l.print();
+    }
+}
